84c01c6ea1ca3ea3030cc52f448cd5a19a62ff77
vladimirmaciel <vladimirmaciel@gmail.com>
2022-12-08 18:35:14 -0300

    criando admin.py e modificando o arquivo model.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Clinete, Produto
+
+
+@admin.register(Clinete)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ("nome", "sobrenome", "email")
+    fields = ("nome", "sobrenome", "email")
+    search_fields = ["sobrenome"]
+
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ("nome", "preco", "estoque")
+    fields = ("nome", "preco", "estoque")
--- core/models.py
+++ core/models.py
@@ -1,4 +1,6 @@
+from django.contrib import admin
 from django.db import models
+from django.utils.html import format_html
 
 
 class Produto(models.Model):
@@ -6,8 +8,14 @@ class Produto(models.Model):
     preco = models.DecimalField("Preço", decimal_places=2, max_digits=8)
     estoque = models.IntegerField("Quantidade em estoque")
 
+    def __str__(self) -> str:
+        return f"{self.nome} {self.estoque} {self.preco}"
+
+
 class Clinete(models.Model):
-    nome = models.CharField('Nome', max_length=100)
-    sobrenome = models.CharField('Sobrenome', max_length=100)
-    email = models.EmailField('E-mail',max_length=100)
-    
+    nome = models.CharField("Nome", max_length=100)
+    sobrenome = models.CharField("Sobrenome", max_length=100)
+    email = models.EmailField("E-mail", max_length=100)
+
+    def __str__(self) -> str:
+        return f"{self.nome} {self.sobrenome} {self.email}"
